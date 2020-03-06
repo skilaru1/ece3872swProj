ca11df5569305447c0dc5917059757fce1772529
Soohyun Kim <sokim@gatech.edu>
2020-03-06 11:27:54 -0500

    added teensy 3.5 DAC test code

---
+++ HWProj/teensy_code/dac_test/dac_test.ino
@@ -0,0 +1,25 @@
+int i, u;
+
+void setup() {
+  // put your setup code here, to run once:
+  i = 0;
+  u = 4095;
+  Serial.begin(9600);
+  analogWriteResolution(12);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  analogWrite(A21, i);
+  analogWrite(A22, u);
+  i += 10;
+  u -= 10;
+  //delay(50);
+  Serial.println(i);
+  if (i > 4095) {
+    i = 0;
+  }
+  if (u < 0) {
+    u = 4095;
+  }
+}
